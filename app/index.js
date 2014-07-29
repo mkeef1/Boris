2423a0099eefc70da7c75c513eae324d81542168
Chyld Medford <chyld.medford@gmail.com>
2014-07-28 22:17:20 -0500

    added home page

--- app/index.js
+++ app/index.js
@@ -1,5 +1,18 @@
 'use strict';
+var express = require('express');
 var app = express();
 var morgan = require('morgan');
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.listen(2000, function(){
+  console.log('Boris is playing!');
+});
